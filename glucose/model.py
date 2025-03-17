7fc9e93f60778c985c652694b4960f125966735d
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-03-17 10:23:27 +0200

    wip

--- glucose/model.py
+++ glucose/model.py
@@ -1,10 +1,15 @@
-from datetime import UTC, datetime
+from sqlalchemy import Column, DateTime, Double, Integer, func
+from sqlalchemy.orm import declarative_base
 
-from pydantic import PositiveFloat
-from sqlmodel import Field, SQLModel
+Base = declarative_base()
 
 
-class Measurement(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=UTC))
-    weight_in_kg: PositiveFloat
+class Model:
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Measurement(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    recorded_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    weight_in_kg = Column(Double, nullable=False)
